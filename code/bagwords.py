2c3a3f1ecf1959563e29781675e4b881a6ade263
Eric Li <cireli9@gmail.com>
2019-10-24 15:37:06 -0400

    remove stopwords and add train/test

--- code/bagwords.py
+++ code/bagwords.py
@@ -1,4 +1,5 @@
 import re
+from nltk.corpus import stopwords
 
 NUM_REVIEWS = 12500 # count of each class
 TOTAL_REVIEWS = NUM_REVIEWS * 2 # positive + negative
@@ -12,12 +13,13 @@ bag = {}
 fcount = {}
 
 def countreviews(path, i):
+  sw = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
   fullpath = path + "/{0}.txt".format(i + 1)
   f = open(fullpath, "r")
   fcount[fullpath] = {}
   for word in f.read().split():
     word = filterchars(word)
-    if len(word) == 0:
+    if len(word) == 0 or word in sw:
       continue
     if word in bag:
       bag[word] = (bag[word][0] + 1, 0, 0)
--- code/log_reg.py
+++ code/log_reg.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
 from os import listdir
 from os.path import isfile, join
 import os
@@ -15,13 +17,13 @@ mypath = join(os.getcwd() + "/task1/train")
 pos_reviews = [f for f in listdir(mypath + "/positive") if isfile(join(mypath+ "/positive", f))]
 neg_reviews = [f for f in listdir(mypath + "/negative") if isfile(join(mypath+ "/negative", f))]
 
-##check if npy file already exists, otherwise gather data
+#check if npy file already exists, otherwise gather data
 if(os.path.exists("x_train.npy")):
-    x_train, y_train = np.load("x_train.npy"), np.load("y_train.npy")
-    for row in x_train:
+    x, y = np.load("x_train.npy"), np.load("y_train.npy")
+    for row in x:
         assert np.nan not in row
         assert np.inf not in row
-    # print(np.where(x_train >= np.finfo(np.float64).max))
+    # print(np.where(x >= np.finfo(np.float64).max))
     # exit(0)
 else:
     x_train = np.zeros((len(pos_reviews) + len(neg_reviews), 5000), dtype=dtype)
@@ -29,14 +31,23 @@ else:
         x_train[i] = vectorize(mypath+ "/positive" + "/" + pos_reviews[i], word_bag)
     for j in tqdm(range(len(neg_reviews)), desc='-', ncols=80):
         x_train[len(pos_reviews) + j] = vectorize(mypath+ "/negative" + "/" +neg_reviews[j], word_bag)
-    y_train = np.zeros(len(pos_reviews) + len(neg_reviews), dtype=dtype)
-    y_train[:len(pos_reviews)] = 1
-    np.save("x_train.npy", np.nan_to_num(x_train))
-    np.save("y_train.npy", y_train)
 
-reg = LogisticRegressionCV(max_iter=100, verbose=1)
-reg.fit(x_train, y_train)
-print(reg.get_params())
+y = np.zeros(len(pos_reviews) + len(neg_reviews), dtype=dtype)
+y[:len(pos_reviews)] = 1
+np.save("x_train.npy", np.nan_to_num(x))
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+
+# reg = LogisticRegressionCV(max_iter=100)
+# reg.fit(X_train, y_train)
+# labs = reg.predict(X_test)
+# np.save("x_preds.npy", labs)
+# print("accuracy of LR: %i", (sum(np.equal(labs, y_test))/len(y_test)))
+
+nb_reg = GaussianNB()
+nb_reg.fit(X_train, y_train)
+print("accuracy of NB: %i", (sum(np.equal(nb_reg.predict(X_test), y_test))/len(y_test)))
 
 ##get test data
 if(os.path.exists("x_test.npy")):
